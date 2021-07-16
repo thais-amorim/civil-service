971054bbea8900afb17e25848efa14bf61fa0094
Thais Marinho <thasmarinho@gmail.com>
2021-07-15 22:48:44 -0300

    Create spring structure

---
+++ src/main/kotlin/com/andoriii/civil/CivilApplication.kt
@@ -0,0 +1,11 @@
+package com.andoriii.civil
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CivilApplication
+
+fun main(args: Array<String>) {
+	runApplication<CivilApplication>(*args)
+}
---
+++ src/test/kotlin/com/andoriii/civil/CivilApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.andoriii.civil
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CivilApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
