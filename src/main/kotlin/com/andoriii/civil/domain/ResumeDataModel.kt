880e3c4426ff28a5d0c807531bc71b423d075e1c
Thais Marinho <thasmarinho@gmail.com>
2021-07-16 00:08:09 -0300

    Add basic controller

--- src/main/kotlin/com/andoriii/civil/domain/ResumeDataModel.kt
+++ src/main/kotlin/com/andoriii/civil/domain/ResumeDataModel.kt
@@ -1,6 +1,11 @@
 package com.andoriii.civil.domain
 
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Table
+
+@Table("SENADOR")
 data class SenadorDataModel(
+    @Id val id: String?,
     val name: String,
     val birthdate: String,
     val politicalParty: String?,
@@ -13,13 +18,23 @@ data class SenadorDataModel(
         fun build(
             name: String,
             birthdate: String,
+            id: String? = null,
             politicalParty: String? = null,
             localBirth: String? = null,
             address: String? = null,
             telephone: String? = null,
             email: String? = null
         ): SenadorDataModel {
-            return SenadorDataModel(name, birthdate, politicalParty, localBirth, address, telephone, email)
+            return SenadorDataModel(
+                id = id,
+                name = name,
+                birthdate = birthdate,
+                politicalParty = politicalParty,
+                localBirth = localBirth,
+                address = address,
+                telephone = telephone,
+                email = email
+            )
         }
     }
 }
---
+++ src/main/kotlin/com/andoriii/civil/domain/repository/ResumeDataRepoditory.kt
@@ -0,0 +1,4 @@
+package com.andoriii.civil.domain.repository
+
+interface ResumeDataRepoditory {
+}
